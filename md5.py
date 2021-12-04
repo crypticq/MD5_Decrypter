9d2c77f9ff70bdf726f7ec26c4bfbc67af85282d
crypticq <85430867+crypticq@users.noreply.github.com>
2021-12-04 09:00:48 +0300

    Update md5.py

--- md5.py
+++ md5.py
@@ -3,37 +3,53 @@ import hashlib
 import sys
 import time
 
+class style():
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+
+
 
 def Banner():
-    print("Usage : Python3 md5.py hash password file")
-    print("""
+    print(style.RED+"Usage : Python3 md5.py hash password file")
+    print(style.CYAN+"""
 
 
         MD5 Cracker Coded By Eng Yazeed
 
-
-                       ...
-             ;::::;
-           ;::::; :;
-         ;:::::'   :;
-        ;:::::;     ;.
-       ,:::::'       ;           OOO\
-       ::::::;       ;          OOOOO\
-       ;:::::;       ;         OOOOOOOO
-      ,;::::::;     ;'         / OOOOOOO
-    ;:::::::::`. ,,,;.        /  / DOOOOOO
-  .';:::::::::::::::::;,     /  /     DOOOO
- ,::::::;::::::;;;;::::;,   /  /        DOOO
-;`::::::`'::::::;;;::::: ,#/  /          DOOO
-:`:::::::`;::::::;;::: ;::#  /            DOOO
-::`:::::::`;:::::::: ;::::# /              DOO
-`:`:::::::`;:::::: ;::::::#/               DOO
- :::`:::::::`;; ;:::::::::##                OO
- ::::`:::::::`;::::::::;:::#                OO
- `:::::`::::::::::::;'`:;::#                O
-  `:::::`::::::::;' /  / `:#
-   ::::::`:::::;'  /  /   `#
-
+                                                                   _...
+                                                       _.'`       -_ ``.
+                                                   .-'`                 `.
+                                                .-`                     q ;
+                                             _-`                       __  \
+                                         .-'`                  . ' .   \ `;/
+                                     _.-`                    /.      `._`/
+                             _...--'`                        \_`..._
+                          .'`                         -         `'--:._
+                       .-`                           \                  `-.
+                      .                `              `-..__.....----...., `.
+                     '                 `  '''---..-''`'              : :  : :
+                   .` -                '`.  `-.                       `'   `'
+                .-` .` '             .`'.__ ;
+            _.-` .-`   '            .
+        _.-` _.-`    .' '         .`
+(`''--'' _.-`      .'  '        .'
+ `'----''        .'  .`       .`
+               .'  .'     .-'`
+             .'   :    .-`
+             `. .`   ,`
+              .'   .'
+             '   .`
+            '  .`
+            `  '.
+            `.___;
 
 
      """)
@@ -44,8 +60,8 @@ def crack_MD5_Hash(hash_to_crack, wordlist):
     for password in file:
         md5_hash = (password.strip("\n")).encode('UTF-8')
         if hashlib.md5(md5_hash).hexdigest() == hash_to_crack:
-            
-            return f"password Cracked ! {password}"
+            print(style.CYAN +"**************************************")
+            return f"password Cracked ! >> Password is >>> {password}"
             return None
 
 
@@ -54,7 +70,7 @@ if __name__ == '__main__':
 
     hash_to_crack = sys.argv[1]
     
-    wordlist = "/opt/SecLists/Passwords/darkweb2017-top10000.txt"
+    wordlist = sys.argv[2]
     
 
     Banner()
